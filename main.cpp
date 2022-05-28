069511b55cd0fdb8854ed246775e71dbc52c908d
Yildirim <fy@smartlockers.io>
2022-05-28 10:59:58 +0200

    Jwt Decoder

--- main.cpp
+++ main.cpp
@@ -5,16 +5,17 @@
 #include <QDebug>
 
 int main() {
-    std::string token = "your token";
+    std::string token = getenv("token");
     auto decoded = jwt::decode(token);
 
     for(auto& e : decoded.get_payload_claims())
         if (e.first == "role")
         {
-            auto tableau = e.second.as_array(); 
+            auto tableau = e.second.as_array();
             std::cout << tableau[0] << std::endl;
-            std::cout << tableau[1] << std::endl; 
+            std::cout << tableau[1] << std::endl;
             QString val = QString::fromStdString(tableau[0].to_str());
-            qDebug() << val << endl;     
-        }    
+            qDebug() << val << endl;
+        }
 }
+
--- project.pro
+++ project.pro
@@ -10,7 +10,7 @@ CONFIG -= app_bundle
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-SOURCES += Untitled-1.cpp
+SOURCES += main.cpp
 
 HEADERS += jwt.h \
         base.h \
